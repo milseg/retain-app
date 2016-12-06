08e41b56f7ab02554c6d1672fce8732bcc91feab
milseg2 <mltnczr@gmail.com>
2016-12-06 16:44:32 -0300

    Feito

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <div>
+      <h3> Olá mundo </h3>
+      <main-container></main-container>
+    </div>
+  `
+})
+export class App {};
---
+++ src/app/containers/main.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'main-container',
+  template: `
+    <div>
+      <main class="main">
+        Conteudo principal
+      </main>
+    </div>
+  `
+})
+export class Main {};
