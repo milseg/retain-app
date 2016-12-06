60ea0d461bc0c169b6597db730cab30e6d6029fe
milseg2 <mltnczr@gmail.com>
2016-12-06 17:30:43 -0300

    Added UI Component AppBar

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app',
   template: `
-    <div>
-      <h3> Olá mundo </h3>
       <main-container></main-container>
-    </div>
   `
 })
 export class App {};
--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -4,7 +4,8 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'main-container',
   template: `
-    <div>
+    <div class="main-container">
+      <app-bar></app-bar>
       <main class="main">
         Conteudo principal
       </main>
