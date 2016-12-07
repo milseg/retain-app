dab46f16d01bd6494c55ad1075a4a15567545b6c
milseg2 <mltnczr@gmail.com>
2016-12-07 17:47:43 -0300

    note-creator: Arquivo adicionado

---
+++ src/app/ui/note-creator.ts
@@ -0,0 +1,87 @@
+import {
+  Component,
+  Output,
+  EventEmitter
+} from '@angular/core';
+
+@Component({
+  selector: 'note-creator',
+  styles: [`
+    .note-creator {
+      padding: 20px;
+      background-color: white;
+      border-radius: 3px;
+    }
+    .title {
+      font-weight: bold;
+      color: rgba(0,0,0,0.8);
+    }
+    .full {
+      height: 100px;
+    }
+  `],
+  template: `
+  <div class="note-creator shadow-2">
+      <form class="row" (submit)="onCreateNote(newNote)">
+        <input
+          type="text"
+          [(ngModel)]="newNote.title"
+          name="newNoteTitle"
+          placeholder="Title"
+          class="col-xs-10 title"
+          *ngIf="expandForm"
+        >
+        <input
+          type="text"
+          [(ngModel)]="newNote.value"
+          name="newNoteValue"
+          placeholder="Take a note..."
+          class="col-xs-10"
+          (focus)="toggle(!expandForm)"
+        >
+        <div class="actions col-xs-12 row between-xs"
+             *ngIf="expandForm">
+          <button
+            type="submit"
+            class="btn-light"
+           >
+            Criar
+          </button>
+          <button
+            type="button"
+            class="btn-light"
+            (click)="reset(newNote)"
+           >
+            Resetar
+          </button>
+        </div>
+      </form>
+      <pre>{{newNote | json}}</pre>
+    </div>
+  `
+})
+export class NoteCreator {
+  @Output() createNote = new EventEmitter();
+  expandForm: boolean = false;
+  newNote = {
+    title: '',
+    value: ''
+  };
+  toggle(val){
+    this.expandForm = val;
+  };
+  onCreateNote(data){
+    const {title, value} = data;
+    if(!title || !value) {
+      console.log("title or value empty");
+      return;
+    }
+    console.log("emmit create note", data);
+    this.createNote.next(Object.assign({}, data));
+    this.reset(data);
+  };
+  reset(data){
+    data.title = '';
+    data.value = '';
+  };
+};
