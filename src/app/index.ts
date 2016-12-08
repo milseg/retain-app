6c3e279988198f2ccb11c0e48cae870b059d26d1
milseg2 <mltnczr@gmail.com>
2016-12-08 17:47:15 -0300

    Added Api Service for http backends using restful guidelines

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,5 @@
 export {App} from './app'
+import * as services from './services';
+const mapValuesToArray = obj => Object.keys(obj).map(key => obj[key]);
+
+export const providers = mapValuesToArray(services);
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {FormsModule} from '@angular/forms';
-import {App} from './app';
+import {HttpModule} from '@angular/http';
+import {App, providers} from './app';
 import {MainContainer, NotesContainer} from './app/containers';
 import {AppBar, NoteCard, NoteCreator, ColorPicker} from './app/ui';
 
@@ -16,7 +17,8 @@ import {AppBar, NoteCard, NoteCreator, ColorPicker} from './app/ui';
     NoteCreator,
     ColorPicker
   ],
-  imports: [BrowserModule, FormsModule],
+  providers,
+  imports: [BrowserModule, FormsModule, HttpModule],
   bootstrap: [App]
 })
 export class AppModule {};
